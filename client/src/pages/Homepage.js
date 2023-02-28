5c2bb47440f000afa4af36bcc79e429204c1fdd8
Jared Kim <jaredkim01@yahoo.com>
2023-02-28 17:11:00 -0600

    started html returns on components and pages

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+function Homepage() {
+  return (
+    <div>
+      {/* <ProjectCard /> */}
+    </div>
+  )
+};
+
+export default Homepage;
