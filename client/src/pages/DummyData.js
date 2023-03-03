6ec6812674501b9ffed3e42b06094cf32cc65334
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-03-03 13:12:33 -0600

    added Homepage tile, Project details, style changes

---
+++ client/src/pages/DummyData.js
@@ -0,0 +1,7 @@
+export const data=[
+    {
+        title:"Weather app",
+        Description:"Provides weather information",
+        funding:"40"
+    },
+]
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { data } from './DummyData';
 // import { useQuery } from '@apollo/client';
 // import background from '../assets/background-mobile.jpg'
 // import AboutUs from '../components/AboutUs';
-
+import { Progress } from 'semantic-ui-react'
 import Auth from '../utils/auth';
 
 function Homepage() {
+
   return (
     <div>
       <h1>Help fund or collaborate on projects!</h1>
@@ -24,10 +26,27 @@ function Homepage() {
               <button>+ New Project</button>
             </Link>
           </>
-        )}
+         )}
         
       </div>
 
+      {data.map((x)=>{
+          return(
+            <div className="MainTile">
+            <h4>Project : {x.title}</h4>
+            <p>
+              Description : {x.Description}
+            </p>
+            <div className='btn-fund-container'>
+            <p>Funding - {x.funding}</p>
+            <button className='button' >Donate</button>
+            </div>
+            <Progress percent={"10"} inverted progress success/>
+            </div>
+          )
+        })
+
+        }
       {/* <img className='background' src={background}></img>
       <AboutUs/> */}
     </div>
