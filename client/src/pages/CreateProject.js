f49233861d2f20a5d99f49c359f535e6513a28d2
Jared Kim <jaredkim01@yahoo.com>
2023-03-02 22:02:05 -0600

    finished forms in CreateProject. need to test

--- client/src/pages/CreateProject.js
+++ client/src/pages/CreateProject.js
@@ -1,35 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+
+import { ADD_PROJECT } from "../utils/mutations";
+import { QUERY_PROJECTS } from "../utils/queries";
+
+import Auth from "../utils/auth";
 
 function CreateProject() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [fundingGoal, setFundingGoal] = useState('');
+
+  const [addProject, { error }] = useMutation(ADD_PROJECT, {
+    update(cache, { data: { addProject } }) {
+      try {
+        const { projects } = cache.readQuery({ query: QUERY_PROJECTS });
+
+        cache.writeQuery({
+          query: QUERY_PROJECTS,
+          data: { projects: [addProject, ...projects] },
+        });
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addProject({
+        variables: {
+          title,
+          description,
+          fundingGoal,
+          //test userID. might not need it here
+          userID: Auth.getProfile().data.username,
+        },
+      });
+
+      setTitle('');
+      setDescription('');
+      setFundingGoal('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'title') {
+      setTitle(value);
+    }
+    if (name === 'description') {
+      setDescription(value);
+    }
+    if (name === 'fundingGoal') {
+      setFundingGoal(value);
+    }
+  };
+
   return (
     <div>
         <h1>Post New Project</h1>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div>
-            <input id="project-title" type="text" name="project-title" placeholder="What is your project title?"/>
+            <textarea 
+              name="title" value={title} id="title" placeholder="What is your project title?"
+              className="" onChange={handleChange}
+            />
           </div> 
 
           <div>
-            <input id="project-description" type="text" name="project-description" placeholder="What is your project description?"/>
+            <textarea 
+              name="description" id="description" placeholder="What is your project description?"
+              className="" onChange={handleChange}
+              />
           </div>  
 
           <div>
-            <input id="project-collab" type="text" name="project-collab" placeholder="Optional: What kind of collaboration is needed?"/>
-          </div>     
+            <label for="fundingGoal">How much funding is needed?</label>
+            <input 
+              name="fundingGoal" id="fundingGoal" type="number" 
+              className="" onChange={handleChange}
+            />
+          </div>   
+          
+          {/* <div>
+            <input id="contributors" type="text" name="contributors" placeholder="Optional: What kind of collaboration is needed?"/>
+          </div>      */} 
+
+          {/* <div>
+            <label for="languages">Languages used:</label>
+            <input id="languages" type="text" name="languages"/>
+          </div> */}
 
           <div>
-            <label for="needed-funding">How much funding is needed?</label>
-            <input id="needed-funding" type="number" name="needed-funding"/>
-          </div>    
-
-          <div>
-            <label>Languages used:</label>
-            <input id="project-languages" type="text" name="project-languages"/>
+            <button type="submit">Post Project</button>
           </div>
 
-          <div>
-            <button>Post Project</button>
-          </div>
+          {error && (
+            <div>
+              {error.message}
+            </div>
+          )}
         </form>
     </div>
   )
