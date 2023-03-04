4ce73111ecac1d71bccd940513040056f8e8595e
RyStreet <ryanstreet122@gmail.com>
2023-03-04 13:12:24 -0600

    projects rendering to user profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Auth from '../utils/auth';
+import { useQuery } from "@apollo/client";
+import {QUERY_ME} from "../utils/queries"
+import ProjectCards from "../components/ProjectCards";
 
 function Profile() {
+  const {loading, data} = useQuery(QUERY_ME)
+  const user = data?.me || []
+  console.log(user)
+  const projects = user.projects
+  console.log(projects)
+
   return (
     <div>
       <div>
         {Auth.loggedIn() ? (
           <>
-            <h2>{Auth.getProfile().data.username}'s Profile</h2>
+            <h2>{user.username}'s Profile</h2>
+            <h5>Email: {user.email}</h5>
             <h5>Bio:</h5>
-            <p>{Auth.getProfile().data.bio}</p>
+            <p>{user.bio}</p>
+
 
             <div>
-              <h4>My Projects:</h4>
-                {/* {userProjects} */}
-            </div>
+        {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ProjectCards
+              projects={projects}
+            />
+          )}
+      </div>
               <div>
                 <h4>Followed Projects:</h4>
                 {/* {followedProjects} */}
