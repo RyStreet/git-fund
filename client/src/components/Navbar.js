74ec059949b3f440b0152f3f097d2343378cc4ff
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-03-06 11:47:20 -0600

    Added style in Login page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../assets/Logo.png';
+import logo from '../assets/logo.png';
 import Homepage from '../pages/Homepage';
 import Auth from '../utils/auth';
 
