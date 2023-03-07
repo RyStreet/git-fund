6fbdfe16c7a205a1f9c3bc37fce4eb1b131c8eae
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-03-07 15:45:15 -0600

    modified profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,18 +4,6 @@ import Auth from '../utils/auth';
 import { useQuery } from "@apollo/client";
 import { QUERY_ME, QUERY_USER } from "../utils/queries"
 import ProjectCards from "../components/ProjectCards";
-import { Card, Image } from "semantic-ui-react";
-const ProjectList = ({ projects }) => {
-  return (
-    <ul>
-      {projects.map((project, index) => (
-        <li key={index}>
-          <a href={project.url}>{project.name}</a>
-        </li>
-      ))}
-    </ul>
-  );
-};
 
 function Profile() {
   const { username: userParam } = useParams();
@@ -31,66 +19,44 @@ function Profile() {
   console.log("collabPROJECT", collabProjects)
 
 
-
-  const [isLoggedIn,setIsLoggedIn]=React.useState(false)
-  React.useEffect(() => {
-    setIsLoggedIn(Auth?.loggedIn())
-    return () => {
-      
-    }
-  }, [Auth])
-  if (loading) {
-    return <div>Loading...</div>;
-  }
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/profile" />
   }
-  
-  const projectss = [
-    { name: 'Project 1', url: 'https://github.com/user/project1' },
-    { name: 'Project 2', url: 'https://github.com/user/project2' },
-    { name: 'Project 3', url: 'https://github.com/user/project3' },
-  ];
+  if (!user?.username) {
+    return (
+      <Link to={'/login'}>
+        <h1>Login or sign up to view your profile</h1>
+      </Link>
+    )
+  }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  
-  // return (
-  //   <div>
-  //     <div>
-  //       <h2>{userParam ? `Now Viewing ${user.username}'s` : `Your`} Profile</h2>
-  //       <h5>Email: {user.email}</h5>
-  //       <h5>GitHub:</h5>
-  //       <h5>Bio:</h5>
-  //       <p>{user.bio}</p>
+  return (
+    <div>
+      <div>
+        <h2>{userParam ? `Now Viewing ${user.username}'s` : `Your`} Profile</h2>
+        <h5>Email: {user.email}</h5>
+        <h5>GitHub:</h5>
+        <h5>Bio:</h5>
+        <p>{user.bio}</p>
 
-  //       <div>
-  //         <ProjectCards
-  //           projects={projects}
-  //         />
-  //       </div>
-
-  //       <div>
-  //         <h4>Followed Projects:</h4>
-  //         {/* {followedProjects} */}
-  //       </div>
-
-  //     </div>       
-  //   </div>
-  // )
-  return  (
-   isLoggedIn?
-<>
-<div>
-          <h4>Collaborated Projects:</h4>
+        <div>
           <ProjectCards
-            projects={ user?.collabProjects}
+            projects={projects}
           />
         </div>
-</>
-    :
 
-<Link to={'/login'}>
-<h1>Login or sign up to view your profile</h1>
-</Link>
+        <div>
+          <h4>Collaborated Projects:</h4>
+          <ProjectCards
+            projects={collabProjects}
+          />
+        </div>
+
+      </div>       
+    </div>
   )
 };
 
