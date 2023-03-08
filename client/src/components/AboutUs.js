1dd593c47d4ebdd5f3c38a51922c881047347345
John W <john.weaver94@utexas.edu>
2023-03-07 21:50:46 -0600

    changed emails to reflect our actual ones

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -8,7 +8,7 @@ function AboutUs() {
       We are graduates of the UT Web Development Boot Camp with aspiatrions of working in the tech industry.
       We have all discovered a passion for coding and are excited to continue learning and growing in this field.
       I hope this project shows our dedication to learning, our ability to work together as a team, and our
-      potential as developers. Please enjoy GitFund
+      potential as developers. Please enjoy GitFund!
     </p>
     </div>
   )
