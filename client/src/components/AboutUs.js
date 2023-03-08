f1621d3239d55037fe3da133b59408d9df3901b4
John W <john.weaver94@utexas.edu>
2023-03-07 21:26:13 -0600

    added an about me

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -4,7 +4,13 @@ function AboutUs() {
   return (
     <div className='about-us'>
     <h1>About Us</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, eum consectetur deleniti a sequi ut voluptatem autem, optio asperiores amet enim eos nam. Eveniet, cupiditate enim! Minima, cupiditate esse. Itaque!</p>
+    <p>The four developers for Gitfund are the amazing Merrin Abraham, Jared Kim, Ryan Street, and John Weaver.
+      We are graduates of the UT Web Development Boot Camp with aspiatrions of working in the tech industry.
+      We have all discovered a passion for coding and are excited to continue learning and growing in this field.
+      I hope this project shows our dedication to learning, our ability to work together as a team, and our
+      potential as developers. Please enjoy 
+    </p>
+    <h2>Gitfund.</h2> <img src='./assets/images/gitfund.png' alt='Gitfund Logo' />
     </div>
   )
 }
