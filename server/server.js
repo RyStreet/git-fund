f6f2926f31f9283c5d02ccbf9507b293c728c662
RyStreet <ryanstreet122@gmail.com>
2023-03-28 16:22:33 -0500

    serving /*

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 
