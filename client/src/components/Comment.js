4328fbd86b4125c0d39bb2bb96031a9823867327
RyStreet <ryanstreet122@gmail.com>
2023-04-24 14:40:18 -0500

    debugging remove comment route

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,7 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../utils/auth'
+import Button from 'react-bootstrap/Button';
+
+import { REMOVE_COMMENT } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { QUERY_SINGLE_PROJECT } from '../utils/queries';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
+
+
 
 const Comment = ({ comments = [] }) => {
+
+const [removeComment, {error}] = useMutation(REMOVE_COMMENT)
+
+const { projectId } = useParams();
+
+const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
+  variables: { projectId: projectId }
+});
+
+const project = data?.project || {};
+
+console.log(project.comments)
+const handleRemoveComment = async(event) => {
+  event.preventDefault()
+  console.log("Comment removed")
+
+  try{
+    const {data} = await removeComment({
+      variables: {
+        projectId: project._id,
+        comment: comment._id
+      }
+
+    })
+    
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+
   if (!comments.length) {
     return (
       <div className='comments'>
@@ -10,22 +53,53 @@ const Comment = ({ comments = [] }) => {
     )
   }
 
+  
+
+
+
+
+  
+
+  
+
+
+  
+
   return (
     <>
       <div>
         {comments &&
           comments.map((comment) => (
+            console.log(comment._id),
+            
+
             <div key={comment._id}>
               <div className='comments'>
                 <p className="card-header" style={{fontWeight:"bold", marginBottom:"5px"}}>Commented by: <Link className="textDecNone" to={`/profiles/${comment.commentAuthor}`}>{comment.commentAuthor}</Link></p>
                 <p>{comment.commentText}</p>
-                    
+
+                {comment.commentAuthor == Auth.getProfile().data.username ? (
+                <div>
+                  <Button variant="danger" onClick={handleRemoveComment} >Delete</Button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
               </div>
+
+              
+
             </div>
+            
           ))}
       </div>
     </>
   );
 };
+
+
+
+
 
 export default Comment;
